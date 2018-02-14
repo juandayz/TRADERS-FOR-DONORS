e9035de999f5139c6a1d68000396bf965bf95579
juan <17104227+juandayz@users.noreply.github.com>
2018-02-14 15:25:55 -0300

    Add files via upload

---
+++ donors.hpp
@@ -0,0 +1,53 @@
+//items
+class Category_1050 {
+	class FoodbaconCooked {
+		type = "trade_items";
+		buy[] = {50,"worth"};
+		sell[] = {8,"worth"};
+	};
+};	
+
+//backpacks
+class Category_1051 {
+class DZ_Czech_Vest_Pouch {
+		type = "trade_backpacks";
+		buy[] = {10,"worth"};
+		sell[] = {5,"worth"};
+	};
+};
+
+//tools
+class Category_1052 {
+class ItemWatch {
+		type = "trade_weapons";
+		buy[] = {20,"worth"};
+		sell[] = {1,"worth"};
+	};
+};
+
+//weapons
+class Category_1053 {
+class M16A4_ACOG_DZ {
+		type = "trade_weapons";
+		buy[] = {30,"worth"};
+		sell[] = {5,"worth"};
+	};
+};	
+
+//Ammo
+class Category_1054 {
+class 30Rnd_556x45_Stanag {
+		type = "trade_items";
+		buy[] = {15,"worth"};
+		sell[] = {3,"worth"};
+	};
+};
+
+//land vehicles //boats helis and planes
+class Category_1055 {
+class Ikarus {
+        type = "trade_any_vehicle";
+        buy[] = {20,"worth"};
+        sell[] = {5,"worth"};
+    };
+};
